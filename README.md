933fccda6439d2f0606d8479ea3bdeade8c5a142
Jack Brown <jackbro@cisco.com>
2024-01-08 21:54:12 +0000

    Docker support.

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-<h1 align="center">Growatt-SPH-Charge-Controller</h1>
+<h1 align="center">Growatt SPH Charge Controller</h1>
 
 <p align="center">
 <a href="https://github.com/psf/black"><img alt="Code style: black" src="https://img.shields.io/badge/code%20style-black-000000.svg"></a>
@@ -6,8 +6,14 @@
 
 <h2>Project Purpose</h2>
 
-To provide an easy method of charging batteries connected to a Growatt SPH inverter using MQTT.
+To provide an easy method of charging batteries connected to a Growatt SPH inverter using MQTT. This has only been tested on my Growatt SPH5000 with three GBLI6532 6.5kWh batteries connected. I suspect this will work for other Growatt hybrid inverters too.
+
+<h2>Docker</h2>
+docker-compose example using published image: docker-compose.yml
+
+docker-compose example with image built locally: docker-compose.dev.yml
 
 <h3>Inspirations and Credits</h3>
 
 - https://github.com/jackbrown1993/growatt-weather-based-charger
+- Tommy Fer over at https://community.home-assistant.io/t/esphome-modbus-growatt-shinewifi-s/369171/323
--- app/app.py
+++ app/app.py
@@ -1,6 +1,8 @@
 import os
 import sys
 import logging
+import asyncio
+from pymodbus.client.sync import ModbusTcpClient
 
 import paho.mqtt.client as mqtt
 
@@ -51,6 +53,33 @@ else:
     mqtt_port = int(os.environ.get("MQTT_PORT"))
 
 
+async def start_mqtt():
+    global mqtt_client
+    mqtt_client = mqtt.Client("growatt-charge-client")
+    mqtt_client.username_pw_set(username=mqtt_user, password=mqtt_password)
+    mqtt_client.on_connect = on_connect
+    mqtt_client.on_message = on_message
+    mqtt_client.connect(mqtt_ip, mqtt_port)
+    mqtt_client.loop_start()
+
+    mqtt_client.publish(
+        "growatt_charge_script/charge_state/actual",
+        payload="unknown",
+        qos=0,
+        retain=False,
+    )
+
+    mqtt_client.publish(
+        "growatt_charge_script/charge_state/desired",
+        payload="unknown",
+        qos=0,
+        retain=False,
+    )
+
+    # Subscribe to MQTT
+    mqtt_client.subscribe("growatt_charge_script/charge_state/desired")
+
+
 def on_connect(mqttc, obj, flags, rc):
     """This is triggered whenever we connect to MQTT"""
     log.info("Connected to MQTT.")
@@ -65,22 +94,78 @@ def on_message(mqttc, obj, msg):
         + " with value: "
         + msg.payload.decode()
     )
-    if msg.topic == "homie/growatt/set_charge-state/set":
+    if msg.topic == "growatt_charge_script/charge_state/desired":
         new_state = msg.payload.decode()
 
         if (new_state == 'charge'):
-            start_charge()
+            log.info("Received request to start charging batteries.")
+            charge_battery()
+        elif (new_state == 'discharge'):
+            log.info("Received request to stop charging batteries.")
+            discharge_battery()
         else:
-            stop_charge
+            log.info("Unhandled MQTT message on topic {}.".format(msg.topic))
     else:
         log.debug("Unhandled MQTT message on topic {}.".format(msg.topic))
 
 
-def start_charge():
-    # Tell SPH to immediately start charging batteries
-    return True
+def charge_battery():
+    try:
+        # Connect to the Modbus TCP server (RS485 to TCP gateway)
+        client = ModbusTcpClient(inverter_ip, port=inverter_port)
+
+        on = [0, 23 * 256 + 59, 1]
+        off = [0, 23 * 256 + 59, 0]
+
+        # BF1
+        client.write_registers(1100, on, unit=1)
+        # LF1
+        client.write_registers(1110, off, unit=1)
+        # GF1
+        client.write_registers(1080, off, unit=1)
+    except Exception as e:
+        print(f"Error: {e}")
+
+    finally:
+        # Close the Modbus TCP connection
+        if client:
+            client.close()
 
 
-def stop_charge():
-    # Tell SPH to immediately stop charging batteries
-    return True
+def discharge_battery():
+    try:
+        # Connect to the Modbus TCP server (RS485 to TCP gateway)
+        client = ModbusTcpClient(inverter_ip, port=inverter_port)
+
+        on = [0, 23 * 256 + 59, 1]
+        off = [0, 23 * 256 + 59, 0]
+
+        # BF1
+        client.write_registers(1100, off, unit=1)
+        # LF1
+        client.write_registers(1110, on, unit=1)
+        # GF1
+        client.write_registers(1080, off, unit=1)
+    except Exception as e:
+        print(f"Error: {e}")
+
+    finally:
+        # Close the Modbus TCP connection
+        if client:
+            client.close()
+
+
+async def start_app():
+    global mqtt_client
+    # Connect to MQTT
+    await start_mqtt()
+
+    try:
+        # Run the event loop indefinitely to keep the script alive
+        while True:
+            await asyncio.sleep(1)  # Adjust sleep duration as needed
+    finally:
+        mqtt_client.disconnect()
+
+if __name__ == "__main__":
+    asyncio.run(start_app())
